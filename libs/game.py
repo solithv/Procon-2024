0cb9cf485476ed52120cf618797bd8ca8ec9967f
solithv <120559288+solithv@users.noreply.github.com>
2024-05-20 18:16:00 +0900

    add: NW row two piece replace

--- libs/game.py
+++ libs/game.py
@@ -1,5 +1,5 @@
 from .patterns import CuttingDie, Board
-from .data import CuttingInfo
+from .data import CuttingInfo, Cell, Direction
 
 
 class Game:
@@ -25,13 +25,39 @@ class Game:
             )
 
     def make_standard_dies(self):
-        self.dies = []
+        self.dies: list[CuttingDie] = []
         for i in range(9):
             for j in range(3):
                 id = 3 * i + j - 2 * bool(i)
-                self.dies.append(CuttingDie.make_standard(id, 2**i, j + 1))
+                self.dies.append(CuttingDie.make_standard(id=id, size=2**i, type=j + 1))
                 if i < 1:
                     break
 
     def check_board(self):
         return self.board.field == self.goal.field
+
+    def search_static_die(self, size, type):
+        for die in self.dies:
+            if die.width == die.height == size and die.type == type:
+                return die
+
+    def row_two_piece_replace(self, corner_target: Cell, target: Cell):
+        # 左上のみ対応(仮)
+        for i in reversed(range(9)):
+            size = 2**i
+            while True:
+                if size <= target.x - corner_target.x - 1:
+                    self.board.apply_die(
+                        self.search_static_die(size, 1),
+                        Cell(target.x - size, target.y),
+                        Direction.right,
+                    )
+                    corner_target.x += size
+                else:
+                    break
+        if target.x - corner_target.x == 1:
+            self.board.apply_die(
+                self.search_static_die(size, 1),
+                Cell(target.x, target.y),
+                Direction.right,
+            )
--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-from libs import Game, PunchingDie, Direction
+from libs import Game, Direction
+from libs.data import Cell
 
 
 def main():
-    board_input = {
-        "width": 6,
-        "height": 4,
-        "start": ["220103", "213033", "022103", "322033"],
-        "goal": ["000000", "111222", "222233", "333333"],
+    sample_input = {
+        "board": {
+            "width": 6,
+            "height": 4,
+            "start": ["012345", "213033", "022103", "322033"],
+            "goal": ["000000", "111222", "222233", "333333"],
+        },
+        "general": {
+            "n": 2,
+            "patterns": [
+                {"p": 25, "width": 4, "height": 2, "cells": ["0111", "1001"]},
+                {"p": 26, "width": 2, "height": 2, "cells": ["10", "01"]},
+            ],
+        },
     }
-    game = Game(
-        board_input["width"],
-        board_input["height"],
-        board_input["start"],
-        board_input["goal"],
-    )
+    game = Game(sample_input)
+    # print(len(game.dies))
 
-    die = PunchingDie(4, 2, ["1111", "1111"])
-    game.board.apply_die(die, 1, 1, Direction.left)
+    # game.board.apply_die(game.dies[26], Cell(1, 1), Direction.left)
+    print(game.board.field)
+    game.row_two_piece_replace(Cell(0, 0), Cell(5, 0))
+    print(game.board.field)
 
 
 if __name__ == "__main__":

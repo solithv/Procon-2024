7d29e7aaf9501270f41e5a38a7d32f67ef50418b
solithv <120559288+solithv@users.noreply.github.com>
2024-05-21 11:09:43 +0900

    refactor: indexes to Cell

--- libs/patterns.py
+++ libs/patterns.py
@@ -70,42 +70,42 @@ class Board(Pattern):
         if -cell.x >= die.width or -cell.y >= die.height:
             raise ValueError
 
-        x_start = 0 if cell.x < 0 else cell.x
-        die_x_start = -cell.x if cell.x < 0 else 0
-        y_start = 0 if cell.y < 0 else cell.y
-        die_y_start = -cell.y if cell.y < 0 else 0
-        x_end = self.width if self.width < die.width + cell.x else die.width + cell.x
-        die_x_end = (
-            self.width - cell.x if self.width < die.width + cell.x else self.width
+        mask_start = Cell(x=0 if cell.x < 0 else cell.x, y=0 if cell.y < 0 else cell.y)
+        mask_end = Cell(
+            x=self.width if self.width < die.width + cell.x else die.width + cell.x,
+            y=self.height if self.height < die.height + cell.y else die.height + cell.y,
         )
-        y_end = (
-            self.height if self.height < die.height + cell.y else die.height + cell.y
-        )
-        die_y_end = (
-            self.height - cell.y if self.height < die.height + cell.y else self.height
+        die_start = Cell(x=-cell.x if cell.x < 0 else 0, y=-cell.y if cell.y < 0 else 0)
+        die_end = Cell(
+            x=self.width - cell.x if self.width < die.width + cell.x else self.width,
+            y=(
+                self.height - cell.y
+                if self.height < die.height + cell.y
+                else self.height
+            ),
         )
 
         mask = np.full_like(self.field, False, dtype=np.bool_)
-        mask[y_start:y_end, x_start:x_end] = die.field[
-            die_y_start:die_y_end, die_x_start:die_x_end
+        mask[mask_start.y : mask_end.y, mask_start.x : mask_end.x] = die.field[
+            die_start.y : die_end.y, die_start.x : die_end.x
         ]
         if direction in (Direction.up, Direction.down):
-            for _x in range(x_start, x_end):
-                temp = self.field[:, _x]
+            for x in range(mask_start.x, mask_end.x):
+                temp = self.field[:, x]
                 if direction == Direction.up:
-                    self.field[:, _x] = np.concatenate(
-                        [temp[~mask[:, _x]], temp[mask[:, _x]]]
+                    self.field[:, x] = np.concatenate(
+                        [temp[~mask[:, x]], temp[mask[:, x]]]
                     )
                 else:
-                    self.field[:, _x] = np.concatenate(
-                        [temp[mask[:, _x]], temp[~mask[:, _x]]]
+                    self.field[:, x] = np.concatenate(
+                        [temp[mask[:, x]], temp[~mask[:, x]]]
                     )
 
         else:
-            for _y in range(y_start, y_end):
-                temp = self.field[_y]
+            for y in range(mask_start.y, mask_end.y):
+                temp = self.field[y]
                 if direction == Direction.left:
-                    self.field[_y] = np.concatenate([temp[~mask[_y]], temp[mask[_y]]])
+                    self.field[y] = np.concatenate([temp[~mask[y]], temp[mask[y]]])
                 else:
-                    self.field[_y] = np.concatenate([temp[mask[_y]], temp[~mask[_y]]])
+                    self.field[y] = np.concatenate([temp[mask[y]], temp[~mask[y]]])
         return CuttingInfo(p=die.id, x=cell.x, y=cell.y, s=direction)

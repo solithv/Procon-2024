1b1e720162835e4246f52c4d382586652290d04c
solithv <120559288+solithv@users.noreply.github.com>
2024-07-24 14:30:03 +0900

    feature: api class

--- libs/network.py
+++ libs/network.py
@@ -1,14 +1,33 @@
+import os
+
 import requests
+from dotenv import load_dotenv
 
 
-def get(url, token):
-    response = requests.get(url, params={"token": token})
-    if response.status_code != 200:
-        raise Exception(f"get failed with {response.status_code}")
-    return response.json()
+class API:
+    def __init__(self) -> None:
+        load_dotenv(".env")
+        token = os.getenv("TOKEN")
+        self.api_url = os.getenv("URL")
+        if self.api_url.endswith("/"):
+            self.api_url = self.api_url[:-1]
+        self.params = {"token": token}
 
+    def get_problem(self) -> dict:
+        """問題取得
 
-def post(url, token, data):
-    response = requests.post(url, json=data, params={"token": token})
-    if response.status_code != 200:
-        raise Exception(f"post failed with {response.status_code}")
+        Returns:
+            dict: 問題json
+        """
+        response = requests.get(f"{self.api_url}/problem", params=self.params)
+        if response.status_code != 200:
+            raise Exception(f"get problem failed with {response.status_code}")
+        return response.json()
+
+    def post_answer(self, data: dict):
+        response = requests.post(
+            f"{self.api_url}/answer", params=self.params, json=data
+        )
+        if response.status_code != 200:
+            raise Exception(f"get problem failed with {response.status_code}")
+        return response.json()

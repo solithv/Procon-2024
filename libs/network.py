18090b235a7fc82524ad51de36cc9a20613825bd
solithv <120559288+solithv@users.noreply.github.com>
2024-07-24 14:39:56 +0900

    fix: exception message

--- libs/network.py
+++ libs/network.py
@@ -1,12 +1,13 @@
 import os
+from typing import Any
 
 import requests
 from dotenv import load_dotenv
 
 
 class API:
-    def __init__(self) -> None:
-        load_dotenv(".env")
+    def __init__(self, env_file: str = ".env") -> None:
+        load_dotenv(env_file)
         token = os.getenv("TOKEN")
         self.api_url = os.getenv("URL")
         if self.api_url.endswith("/"):
@@ -16,18 +17,36 @@ class API:
     def get_problem(self) -> dict:
         """問題取得
 
+        Raises:
+            HTTPError: Get失敗
+
         Returns:
-            dict: 問題json
+            dict: json形式問題
         """
         response = requests.get(f"{self.api_url}/problem", params=self.params)
         if response.status_code != 200:
-            raise Exception(f"get problem failed with {response.status_code}")
+            raise requests.HTTPError(
+                f"get problem failed with status code {response.status_code}: {response.text}"
+            )
         return response.json()
 
-    def post_answer(self, data: dict):
+    def post_answer(self, data: dict) -> Any:
+        """回答提出
+
+        Args:
+            data (dict): 回答データ
+
+        Raises:
+            HTTPError: Post失敗
+
+        Returns:
+            Any: レスポンスメッセージ
+        """
         response = requests.post(
             f"{self.api_url}/answer", params=self.params, json=data
         )
         if response.status_code != 200:
-            raise Exception(f"get problem failed with {response.status_code}")
+            raise requests.HTTPError(
+                f"post answer failed with status code {response.status_code}: {response.text}"
+            )
         return response.json()
